a5c25d90dba6fc9dca60d1cab96b26e769098386
Kin <kinfurigay.priv@gmail.com>
2025-01-15 17:19:49 -0500

    Finished setting up the home layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+
+export default function Home() {
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
+}
