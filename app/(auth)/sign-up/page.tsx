425c06ebd94688bf4dc945592482c317131e81f6
Kin <kinfurigay.priv@gmail.com>
2025-01-16 03:37:34 -0500

    Finished setting up auth ui

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signUpSchema } from "@/lib/validations";
+
+const Page = () => {
+  return (
+    <AuthForm
+      type="SIGN_UP"
+      schema={signUpSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+        fullName: "",
+        universityId: 0,
+        universityCard: "",
+      }}
+      onSubmit={() => {}}
+    />
+  );
+};
+
+export default Page;
