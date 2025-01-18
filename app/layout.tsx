1929bad95f7cffe2cdac5a601d97fd7ce98f5ed0
Kin <kinfurigay.priv@gmail.com>
2025-01-18 07:28:20 -0500

    Finished setting up ImageKit for sign up image upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
 
 const ibmPlexSans = localFont({
   src: [
@@ -32,6 +33,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  env: {
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    imageKit: {
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      privateKey: process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+    },
+  },
+};
+
+export default config;
