591541b5a2b70d7c2e2e4567db7a875b03687b82
Kin <kinfurigay.priv@gmail.com>
2025-01-19 15:27:25 -0500

    Successfully setup user onboarding flow with qstash

--- lib/config.ts
+++ lib/config.ts
@@ -10,6 +10,8 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
       redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
   },
 };
